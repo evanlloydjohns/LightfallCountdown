c26e8bcda177405ebefa70f2cbaf90dfe012cda3
Evan Johns <evanlloydjohns@gmail.com>
2023-02-17 23:12:19 -0500

    Add project files.

---
+++ ContextMenus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using CountdownApp.Properties;
+using System.Drawing;
+
+namespace CountdownApp
+{
+    internal class ContextMenus
+    {
+
+        public ContextMenuStrip Create()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+            ToolStripMenuItem exitItem = new ToolStripMenuItem();
+            exitItem.Text = "Close";
+            exitItem.Click += new EventHandler(Exit_Click);
+            exitItem.Image = Resources.Exit;
+            menu.Items.Add(exitItem);
+
+            return menu;
+        }
+
+        void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ ProcessIcon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using CountdownApp.Properties;
+using System.Resources;
+
+namespace CountdownApp
+{
+    internal class ProcessIcon : IDisposable
+    {
+        NotifyIcon releaseIcon;
+        DateTime releaseDate;
+        TimeSpan timeRemaining;
+
+        public ProcessIcon()
+        {
+            releaseDate = new DateTime(2023, 2, 28, 12, 0, 0);
+            releaseIcon = new NotifyIcon();
+        }
+
+        public void Display()
+        {
+            // Create System Tray Icon
+            releaseIcon.Icon = Resources.Lightfall_Icon_XSmall;
+            releaseIcon.Visible = true;
+
+            // Set Up Timer
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += UpdateTimer;
+            timer.Start();
+
+            // Add Context Menu to System Tray Icon
+            releaseIcon.ContextMenuStrip = new ContextMenus().Create();
+        }
+
+        public void UpdateTimer(object sender, EventArgs e) 
+        {
+            DateTime futureTime = new DateTime(2023, 3, 1, 12, 0, 0); // Replace with your desired future time
+
+            TimeSpan timeRemaining = futureTime - DateTime.Now;
+
+            string timeRemainingText = String.Format(
+                "Light falls in \n{0} days\n{1} hours\n{2} minutes\n{3} seconds", 
+                timeRemaining.Days, 
+                timeRemaining.Hours, 
+                timeRemaining.Minutes, 
+                timeRemaining.Seconds
+            );
+            releaseIcon.Text = timeRemainingText;
+        }
+
+        public void Dispose()
+        {
+            releaseIcon.Dispose();
+        }
+    }
+}
